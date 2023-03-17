195036f6e26780ce5969aee1edc8db73e06129c1
Aditya Hegde <adityahegderocks@gmail.com>
2023-03-17 15:39:09 +0530

    Recovering to usable state

--- models/normalized_311_model.sql
+++ models/normalized_311_model.sql
@@ -1,28 +1,6 @@
 -- @materialize:true
 WITH  
 
-BerkeleyNormalized AS 
-(
-SELECT
-  'Berkeley' AS city,
-  'CA' AS state, 
-  Date_Opened  AS start_event_date,
-  STRPTIME(Date_Closed, '%M/%d/%Y %I:%M:%S %p') AS end_event_date,
-  Longitude AS longitude,
-  Latitude AS latitude,
-  Case_ID AS ticket_id,
-  Street_Address AS street_address,
-  CONCAT(Request_Detail, ': ', Object_Type) AS description, 
-  Request_Category AS category,
-  Request_SubCategory AS activity,
-  Case_Status AS status,
-  NULL AS method,
-  NULL AS outcome,
-  Neighborhood AS neighborhood,
-FROM berkeley
-),
-
-
 OaklandNormalized AS 
 (
 SELECT
@@ -42,27 +20,6 @@ SELECT
   REFERREDTO AS outcome,
   COUNCILDISTRICT AS neighborhood,
 FROM oakland
-),
-
-SanFranciscoNormalized AS 
-(
-SELECT
-  'San Francisco' AS city,
-  'CA' AS state,
-  Opened AS start_event_date,
-  STRPTIME(Closed, '%m/%d/%Y %I:%M:%S %p') AS end_event_date,
-  CASE WHEN Longitude = '0.0' THEN NULL ELSE Longitude END AS longitude,
-  CASE WHEN Latitude = '0.0' THEN NULL ELSE Latitude END  AS latitude,
-  CaseID AS ticket_id,
-  Street AS street_address,
-  CONCAT("Request Type", ': ', "Request Details") AS description, 
-  Category AS category,
-  "Media URL" AS activity,
-  "Status" AS status,
-  Source AS method,
-  "Responsible Agency" AS outcome,
-  Neighborhoods AS neighborhood,
-FROM sanfrancisco
 ),
 
 
@@ -88,11 +45,7 @@ FROM sanjose
 ),
 
 Together AS (
-SELECT * FROM BerkeleyNormalized
-  UNION ALL 
 SELECT * FROM OaklandNormalized
-  UNION ALL 
-SELECT * FROM SanFranciscoNormalized
   UNION ALL 
 SELECT * FROM SanJoseNormalized
 
